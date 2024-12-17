c8400c273618815295050806895ec812f8542847
Neihtq <q.thien.nguyen@outlook.de>
2024-12-17 11:40:24 +0100

    day 17 p1

---
+++ utils/day17/get_input_utils.py
@@ -0,0 +1,10 @@
+from utils.input_path_utils import get_input_path
+
+
+def get_input():
+    input_path = get_input_path(17)
+    with open(input_path) as file:
+        lines = file.readlines()
+        registers = {register : int(lines[i].strip().split(' ')[-1]) for i, register in enumerate([4, 5, 6])}
+        program = [int(num) for num in lines[-1].strip().split(' ')[-1].split(',')]
+        return registers, program
